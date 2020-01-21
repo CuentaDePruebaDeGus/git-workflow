188deed99cdbc8b70ce36df2e532b74bf35dc6ee
Ernestrod <ernesto@circuloconcentrico.com>
2020-01-20 21:26:10 -0300

    added quote

--- division.js
+++ division.js
@@ -2,6 +2,6 @@ function division(divisor, dividendo) {
     if(divisor !== 0) {
         return dividendo / divisor;
     } else {
-        throw new Error('no se puede dividir por 0')
+        throw new Error('no se puede dividir por 0');
     }
 }
