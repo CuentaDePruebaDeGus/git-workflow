66be339124dc4d329d524347a8f1a1d0403f6fc1
Ernestrod <ernesto@circuloconcentrico.com>
2020-01-20 21:18:53 -0300

    change condition

--- division.js
+++ division.js
@@ -1,5 +1,5 @@
 function division(divisor, dividendo) {
-    if(divisor > 0) {
+    if(divisor !== 0) {
         return dividendo / divisor
     } else {
         throw new Error('no se puede dividir por 0')
